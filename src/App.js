497d732f2d586fd952fd1c2f9418021342107186
Matthew Yee <matthew.yeedev@gmail.com>
2019-06-23 19:26:29 -0700

    changed class to function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import React, { Component } from 'react'
+import React from 'react'
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Recipe App</h1>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+  <div>
+    <h1>Recipe App</h1>
+  </div>
+  );
 }
-
-export default App
+export default App;
 
