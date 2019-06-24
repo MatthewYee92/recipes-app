ec4d98447ff7a21682678747fddbbec5465bc7e5
Matthew Yee <matthew.yeedev@gmail.com>
2019-06-23 19:42:13 -0700

    added search form, search button, and search bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import './App.css';
 import React from 'react'
 
 const App = () => {
+
+  // API KEY & ID
+  const APP_ID = '4b179fdc';
+  const APP_KEY = 'ac0b5b9e6eb1f6acce1265b4f9f69f0b';
+
+  // get request NOT WORKING 
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-  <div>
+  <div className='App'>
     <h1>Recipe App</h1>
+    <form className='search-form'>
+      <input className='search-bar' type='text'></input>
+      <button className='search-button' type='submit'>Search</button>
+    </form>
   </div>
   );
 }
